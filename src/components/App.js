6123af9a2caabc727bca08ac34ffcceb80280ee1
Boudewijn Danser <helpdesk@boudewijndanser.nl>
2019-08-31 22:06:09 +0200

    Start...

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,43 @@
 import React, { Component } from 'react'
 import '../css/App.css'
 
+const companies = [
+  {name: "Company One", category: "Finance", start: 1981, end: 2003},
+  {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+  {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+  {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+  {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+  {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+  {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+  {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+  {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+];
+
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      person: {
-        name:undefined,
-        age:'25',
-        hometown:'Antwerp',
-        favourites: {
-          color: 'yellow',
-          food: 'curry'
-        }
-      }
+    this.state ={
+      companies,
+      ages
     }
-  }
-
-  changeState = () => {
-    this.setState({
-      person: {
-        name:'Jack',
-        age:'25',
-        hometown:'Dordrecht',
-        favourites: {
-          color: 'yellow',
-          food: 'curry'
-        }
-      }
-     })
-     console.log('this.state.person: ',this.state.person)
-  }
+    }
+  
+  //forEach
+  
+   companyNames = companies => companies.forEach(company => console.log(company.name))
+   companyName = companies => companies.forEach(company => console.log(company.name))
 
   render() {
-    console.log('this.state.person: ',this.state.person)
-
-    // Destructuring for easy acces
-    let { age, hometown, favourites: {color, food} } = this.state.person 
-    let { name = "default" } = this.state.person
-
-    console.log('Easy acces examples',{name},{age})
-
+    console.log('State: ', this.state)
     return (
       <div className="App">
-        <h4>From state:</h4>
-        <p>My name is {this.state.person.name}, I'm from {this.state.person.hometown} and am {this.state.person.age} years old. You can wake me up for {this.state.person.favourites.food} and paint my walls {this.state.person.favourites.color}</p>
-        <h4>From destructuring:</h4>
-        <p>My name is {name}, I'm from {hometown} and am {age} years old. You can wake me up for {food} and paint my walls {color}</p> 
-        <button onClick={() => {this.changeState()}}>setState</button>
+        <h4>Arrays:</h4>
+        <ul>{this.state.companies.map(company =>
+          <li key={`company--${company.name}`}>{company.name}</li>
+          )}
+        </ul>
       </div>
     )
   }
